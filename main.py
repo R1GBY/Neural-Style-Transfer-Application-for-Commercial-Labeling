4319c442f54ffa14362b27434ecf2aacf1a6d569
Umut Arda Kapan <umutardakapan@gmail.com>
2023-03-26 01:02:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+import os
+from flask import Flask, render_template, request, redirect, url_for
+import PIL
+from PIL import Image
+
+import tensorflow_hub as hub
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# Load the TensorFlow Hub model
+model_url = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+model = hub.load(model_url)
+
+##################################################################################
+#Converting the format that the model (Neural Network) uses to process images to a format that we can visualize
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+
+#This function is giving the user the ability to load images
+
+def load_img(path_to_img):
+  # max dimension of the images that we are importing
+  max_dim = 512
+
+
+#Here we are loading our file, decoding it into a 3 dimensional tensor and converting it into a file based on format
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+##################################################################################
+@app.route('/')
+def index():
+    images = [filename for filename in os.listdir('static/uploads')
+              if filename.endswith('.jpg') or filename.endswith('.png')]
+    return render_template('index.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    max_dim = 512
+
+    content_path = file_path
+    style_path = 'static/styles/pirosmani.jpeg'
+
+    # Loading in our content and style image
+    content_image = load_img(content_path)
+    style_image = load_img(style_path)
+
+    # Run the model on the image and style image
+    stylized_image = model(tf.constant(content_image), tf.constant(style_image))[0]
+    tensor_to_image(stylized_image)
+
+    # save the image
+    stylized_filename = 'stylized_image.jpg'
+
+    saving_path = 'static/uploads/'
+    os.chdir(saving_path)
+    tensor_to_image(stylized_image).save(stylized_filename)
+
+    return render_template('result.html', filename=filename, stylized_filename=stylized_filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
