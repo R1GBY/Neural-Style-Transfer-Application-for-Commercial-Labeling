ca6bea8a032ab2774dda92eef90db66fd092a4a3
Umut Arda Kapan <umutardakapan@gmail.com>
2023-06-09 23:27:32 +0300

    Safer document system and better naming for files

--- application.py
+++ application.py
@@ -1,25 +1,28 @@
 import tensorflow as tf
 import numpy as np
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import PIL
 from PIL import Image
 
+import uuid
+
+
 import tensorflow_hub as hub
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['UPLOAD_FOLDER'] = 'static/images/uploads'
 
 # Load the TensorFlow Hub model
 model_url = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
 model = hub.load(model_url)
 
-def increment_filename(filename):
+def increment_filename(filename): #instead of just name and number we can use date of exact time
   base, ext = os.path.splitext(filename)
-  i = 1
+
   while os.path.exists(filename):
-    filename = f"{base}_{i}{ext}"
-    i += 1
+    filename = f"{base}_{str(uuid.uuid4())}{ext}"
+    print(filename)
   return filename
 
 ##################################################################################
@@ -57,9 +60,15 @@ def load_img(path_to_img):
 ##################################################################################
 @app.route('/')
 def index():
-    images = [filename for filename in os.listdir('static/uploads')
+    images_style = [filename for filename in os.listdir('static/images/style')
               if filename.endswith('.jpg') or filename.endswith('.png')]
-    return render_template('index.html', images=images)
+    images_label = [filename for filename in os.listdir('static/images/label')
+              if filename.endswith('.jpg') or filename.endswith('.png')]
+    images_upload = [filename for filename in os.listdir('static/images/uploads')
+              if filename.endswith('.jpg') or filename.endswith('.png')]
+    images_results = [filename for filename in os.listdir('static/images/results')
+              if filename.endswith('.jpg') or filename.endswith('.png')]
+    return render_template('index.html', images_style=images_style, images_label=images_label, images_results=images_results, images_upload=images_upload )
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -68,15 +77,14 @@ def upload():
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
 
-    label = request.form['label-design']
+    label = 'static/images/label/' + request.form['label-design']
 
     style = request.form['style-image']
-    print(style)
 
     max_dim = 512
 
     content_path = file_path
-    style_path = 'static/uploads/' + style
+    style_path = 'static/images/style/' + style
 
     # Loading in our content and style image
     content_image = load_img(content_path)
@@ -91,15 +99,22 @@ def upload():
 
     stylized_filename = increment_filename("stylized_image.jpg")
 
-    saving_path = 'static/uploads/'
+    saving_path = 'static/images/results/'
+
     oldpwd = os.getcwd()
+    print("oldpwd: ", oldpwd)
     os.chdir(saving_path)
+    print("saving path: ", saving_path)
+    print("label:",label)
     tensor_to_image(stylized_image).save(stylized_filename)
 
-    #merging label and background stylized image
+    # merging label and background stylized image
 
     image1 = Image.open(stylized_filename)
+
+    os.chdir(oldpwd)
     image2 = Image.open(label)
+    os.chdir(saving_path)
 
     width, height = image2.size
     image1 = image1.resize((width, height))
@@ -110,10 +125,16 @@ def upload():
     merged_image.paste(image2, (0, 0), image2)
     merged_image.save(stylized_filename)
 
-    #turning back to old folder
+    # turning back to old folder
     os.chdir(oldpwd)
 
     return render_template('result.html', filename=filename, stylized_filename=stylized_filename)
+
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory('static/images/results', filename, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
